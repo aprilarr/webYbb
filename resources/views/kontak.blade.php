ab379bd2bad9faebd1dd7488bbd64c6a3a0bc793
Aprila Rachmat Riady <aprila.rachmat@gmail.com>
2020-05-04 20:37:44 +0700

    coba phpmailer

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -44,10 +44,6 @@
                     <input type="email" class="form-control" id="email" name="email">
                 </div>
                 <div class="form-group">
-                    <label for="subject">Subject</label>
-                    <input type="text" class="form-control" id="subject" name="subject">
-                </div>
-                <div class="form-group">
                     <label for="message">Message</label>
                     <textarea cols="30" rows="10" class="form-control" id="message" name="message"></textarea>
                 </div>
