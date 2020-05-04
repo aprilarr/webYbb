6a0c41f0901684905f1a98325f8fe1084fddbe68
Aprila Rachmat Riady <aprila.rachmat@gmail.com>
2020-05-04 17:15:12 +0700

    merubah layout.blade.php bagian testimonial

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -286,7 +286,7 @@
           <div class="row">
             <div class="col-md-6 col-md-offset-3 text-center section-heading probootstrap-animate">
               <h2>Alumni Testimonial</h2>
-              <p class="lead">Sed a repudiandae impedit voluptate nam Deleniti dignissimos perspiciatis nostrum porro nesciunt</p>
+              <p class="lead">Testimonial dari alumni mengenai pengalaman selama bersekolah di SMP Yayasan Budi Bakti</p>
             </div>
           </div>
           <!-- END row -->
@@ -297,26 +297,26 @@
 
                   <div class="probootstrap-testimony-wrap text-center">
                     <figure>
-                      <img src="enlight/img/person_1.jpg" alt="Free Bootstrap Template by uicookies.com">
+                      <img src="enlight/img/alumni1.jpg" alt="Free Bootstrap Template by uicookies.com">
                     </figure>
-                    <blockquote class="quote">&ldquo;Design must be functional and functionality must be translated into visual aesthetics, without any reliance on gimmicks that have to be explained.&rdquo; <cite class="author"> &mdash; <span>Mike Fisher</span></cite></blockquote>
+                    <blockquote class="quote">&ldquo;Sungguh seru.....!!!! apalagi saya adalah mantan ketua osis kala itu banyak hal yang mengasyikan dan banyak juga pejaran yang dapat saya ambil.&rdquo; <cite class="author"> &mdash; <span>Hasan Abdul</span></cite></blockquote>
                   </div>
 
                 </div>
                 <div class="item">
                   <div class="probootstrap-testimony-wrap text-center">
                     <figure>
-                      <img src="enlight/img/person_2.jpg" alt="Free Bootstrap Template by uicookies.com">
+                      <img src="enlight/img/alumni2.jpg" alt="Free Bootstrap Template by uicookies.com">
                     </figure>
-                    <blockquote class="quote">&ldquo;Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn’t really do it, they just saw something. It seemed obvious to them after a while.&rdquo; <cite class="author"> &mdash;<span>Jorge Smith</span></cite></blockquote>
+                    <blockquote class="quote">&ldquo;Selama saya berada di SMP YBB itu adalah pengalaman yang sangat membekas dari mulai marah,sedih,gembira, senang semua saya rasakan disini. Semoga YBB selalu tetap juara!!&rdquo; <cite class="author"> &mdash;<span>Feri</span></cite></blockquote>
                   </div>
                 </div>
                 <div class="item">
                   <div class="probootstrap-testimony-wrap text-center">
                     <figure>
-                      <img src="enlight/img/person_3.jpg" alt="Free Bootstrap Template by uicookies.com">
+                      <img src="enlight/img/alumni3.jpg" alt="Free Bootstrap Template by uicookies.com">
                     </figure>
-                    <blockquote class="quote">&ldquo;I think design would be better if designers were much more skeptical about its applications. If you believe in the potency of your craft, where you choose to dole it out is not something to take lightly.&rdquo; <cite class="author">&mdash; <span>Brandon White</span></cite></blockquote>
+                    <blockquote class="quote">&ldquo;Saya alumni angkatan 2014. Saat saya di YBB saya senang karena ada ekstrakulikuler yang saya minati yang selalu mengisi waktu luang saya, Pokonya mengasyikan deh. Hidup YBB...!&rdquo; <cite class="author">&mdash; <span>Ina</span></cite></blockquote>
                   </div>
                 </div>
                 
