de6d7dc3471cba4f2ffb9ed61ad75207d7109d55
Aprila Rachmat Riady <aprila.rachmat@gmail.com>
2020-05-04 16:08:30 +0700

    menyelesaikan halaman view profil

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -15,7 +15,7 @@
         <div class="col-md-12">
             <div class="probootstrap-flex-block">
             <div class="probootstrap-text probootstrap-animate">
-                <div class="text-uppercase probootstrap-uppercase">PROFIL</div>
+                <div class="text-uppercase probootstrap-uppercase">THE TRENDS SCHOOL</div>
                 <h3>SMP YBB</h3>
                 <p>Halaman ini menyediakan informasi SMP Yayasan Budi Bakti secara umum.</p>
                 <br>
@@ -35,13 +35,52 @@
         
         <div class="col-md-6">
         <p>
-            <img src="enlight/img/slider_1.jpg" alt="Free Bootstrap Template by uicookes.com" class="img-responsive">
+            <img src="enlight/img/profil.jpg" alt="Free Bootstrap Template by uicookes.com" class="img-responsive">
         </p>
         </div>
         <div class="col-md-6 col-md-push-1">
-        <h2>We are NYC based School focused on excellence.</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis explicabo veniam labore ratione illo vero voluptate a deserunt incidunt odio aliquam commodi blanditiis voluptas error non rerum temporibus optio accusantium!</p>
-        <p>Quis explicabo veniam labore ratione illo vero voluptate a deserunt incidunt odio aliquam commodi blanditiis voluptas error non rerum temporibus optio accusantium!</p>
+        <h2>Sejarah Singkat</h2>
+        <p>SMP Yayasan Budi Bakti terletak di Kota Bandung tepatnya terletak di Jalan Citepus II No. 3, 
+            Kelurahan Pajajaran, Kecamatan Cicendo. SMP Yayasan Budi Bakti didirikan pada tahun 1980 dan 
+            diberikan izin pada tanggal 24 November 1980 seusai dengan surat keputusan dari Kepala Kantor
+            Wilayah Departemen Pendidikan dan Kebudayaan Provinsi Jawab Barat Nomor. 166 I02.Kep/ E 80. 
+            SMP Yayasan Budi Bakti memiliki lahan seluas 835m^2 dan luas bangunan 695m^2, dengan luas 
+            tanah dan bangunan tersebut SMP Yayasan Budi Bakti memiliki fasilitas yang menunjang seperti 
+            Lab. Komputer, perpustakaan, mushola, lapangan olahraga, ruang kesenian, dan lain-lain. Dengan 
+            fasalitas yang tersedia SMP Yayasan Budi Bakti siap dalam menjalankan tugas untuk membimbing 
+            setiap siswa untuk mencapai lulusan yang terampil, religious, dinamis, cerdas, kompetitif dan santun.</p>
+        </div>
+        <div class="col-md-6 col-md-push-1">
+        <h2>Tujuan</h2>
+        <p>1. Membentuk siswa sampai akhir pendidikan SMP mampu dan terbiasa melaksanakan sholat wajib lima 
+            waktu dan sholat sunnah lainya dan mampu membaca Al-Qur’an, hafal Juz’ama, hafal Asmaul Husna bagi 
+            siswa yang beragama islam serta mampu menerapkan materi pelajaran agam dan pendidikan akhlak mulia.</p>
+        <p>2. Mampu menerapkan norma-norma kehidupan sosial di sekolah dan dimasyarakat seperti menghormati
+            orang tua, guru dan kepada orang yang lebih tua, selalu mengucapkan salam.</p>
+        <p>3. Mampu menghargai dan menerapkan budaya daerah (Budaya Sunda) dengan menggunakan pakaian sunda
+            dan berkomunikasi dengan bahasan sunda setiap hari rabu (rebo nyunda).</p>
+        <p>4.	Membentuk siswa mampu berfikir dan memecahkan permasalahan diri sendiri dan sosial secara ilmiah, 
+            kritis, rasional dan kreatif dalam bermasyarakat sesuai dengan norma agama dan norma sosial.</p>
+        <p>5.	Menghasilkan suswa yang mampu:<br>
+            a.	Berkomunikasi lisan dan tulisan menggunakan bahasa Indonesia yang baik dan benar serta 
+            berkomunikasi lisan dalam bahasa inggris secara singkat dan sederhana dalam kehidupan sehari-hari.<br>
+            b.	Mampu berkomunikasi dengan baik dan benar secara lisan dan tulisan dengan menggunakan bahasa 
+            sunda dalam kehidupan sehari-hari.<br>
+            c.	Menerapkan konsep ilmu pengatahuan alam sehingga dapat membuat alat sederhana yang berguna dalam 
+            kehidupan sehari-hari.<br>
+            d.	Menggunaka teknologi informasi sebagai sarana untuk meningkatkan kualitas sumber daya manusia 
+            dalam ilmu pengetahuan dan teknologi sederhana.
+        </p>
+        <p>6. Memiliki team futsal, volley, tenis meja, basket, rohis, pramuka, marawis, dan murotal.</p>
+        <p>7. Menciptakan lingkungan sekolah kondusif 7K (kebersihan, keamanan, ketertiban, keindahan,
+            kenyamanan, ketentraman, dan kerindangan).</p>
+        <p>8. Terciptanya standar kompentensi kelulusan siswa 100% dengan memenuhi yang ditentukan.</p>
+        <p>9. Terciptanya standar kualifikasi pendidik yang minimal berkualifikasi strata satu (S1) 
+            sesuai dengan mata pelajaran yang diampunya.</p>
+        <p>10. Para pendidik dan tenaga kependidikan mampu menerapkan standar pelayanan minimal baik 
+            dalam proses belajar mengajar.</p>
+        <p>11.	Para pendidik dan tega kependidikan dapat memberi suri tauladan kepada para siswa dalam
+            mengaplikasikan budaya sekolah. </p>
         </div>
     </div>
     </section>
@@ -51,8 +90,9 @@
     <div class="container">
         <div class="row">
         <div class="col-md-6 col-md-offset-3 text-center section-heading probootstrap-animate">
-            <h2>Why Choose Enlight School</h2>
-            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+            <h2>Visi</h2>
+            <p class="lead">Visi SMP Yayasan Budi Bakti. (The Trends School) : menciptakan siswa yang terampil, religius, dinamis, pintar, 
+                sopan santun dalam ilmu pengetahuan berdasarkan dengan indikator:</p>
         </div>
         </div>
         <div class="row">
@@ -60,47 +100,171 @@
             <div class="service left-icon probootstrap-animate">
             <div class="icon"><i class="icon-checkmark"></i></div>
             <div class="text">
-                <h3>Consectetur Adipisicing</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+                <h3>1</h3>
+                <p>Terwujudnya kurikulum yang dinamis, adaptif, proaktif dan kompetitif.</p>
             </div>  
             </div>
             <div class="service left-icon probootstrap-animate">
             <div class="icon"><i class="icon-checkmark"></i></div>
             <div class="text">
-                <h3>Aliquid Dolorum Saepe</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+                <h3>2</h3>
+                <p>Terwujudnya pembelajaran yang kreatif, inovatif, efektif, efesien, santun dan agamis.</p>
             </div>
             </div>
             <div class="service left-icon probootstrap-animate">
             <div class="icon"><i class="icon-checkmark"></i></div>
             <div class="text">
-                <h3>Eveniet Tempora Anisi</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+                <h3>3</h3>
+                <p>Terwujudnya lulusan yang terampil, religius, dinamis, cerdas, kompetitif, dan santun.</p>
             </div>
+            </div>
+
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>4</h3>
+                <p>Terwujudnya tenaga pendidik dan kependidikan, yang berkualifikasi S1 dan unggul dalam pedagogis,
+                    professional, berkepribadian, kreatif, inovatif, cerdas, dinamis, dan enerjik.</p>
+            </div>  
             </div>
         </div>
+        <div class="col-md-6">
+            
+            
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>5</h3>
+                <p>Terwujudnya sarana prasarana yang memadai, indah, asri, sejuk, aman dan berwawasan lingkungan.</p>
+            </div>
+            </div>
+            
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>6</h3>
+                <p>Terwujudnya manajemen berbasis sekolah yang mandiri, amanah, transparan, responsive, 
+                    partipasif, komunikatif, akuntabilitas dengan kepemimpinan yang tangguh bernuansa religius, 
+                    yang berorientasi kepadaya budaya mutu. </p>
+            </div>
+            </div>
+
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>7</h3>
+                <p>Terwujudnya pengelolaan pembiayaan yang mandiri, memadai, efektif, efesien, amanah dan akuntabel. </p>
+            </div>
+            </div>
+            
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>8</h3>
+                <p>Terwujudnya penilaian proses dalam kegiatan pembelajaran yang koprehentif, akurat, edukatif, adil dan berkesinambungan. </p>
+            </div>
+            </div>
+
+        </div>
+        </div>
+        <!-- END row -->
+    </div>
+    </section>
+
+    <section class="probootstrap-section">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-6 col-md-offset-3 text-center section-heading probootstrap-animate">
+            <h2>MISI</h2>
+            <p class="lead">Misi SMP Yayasan Budi Bakti.</p>
+        </div>
+        </div>
+        <div class="row">
         <div class="col-md-6">
             <div class="service left-icon probootstrap-animate">
             <div class="icon"><i class="icon-checkmark"></i></div>
             <div class="text">
-                <h3>Laboriosam Quod Dignissimos</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+                <h3>1</h3>
+                <p>Mewujudkan kurikulum KURTILAS SMP Yayasan Budi Bakti yang berorientasi terhadap pembentukan 
+                    karakter pribadi keilmuan yang berwawasan luas berbasis IPTEK.</p>
             </div>  
             </div>
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>2</h3>
+                <p>Menghantarkan para peserta didik menjadi insan yang agamis, berakhlak mulia dan mampu membaca, 
+                    menulis, mengerti Al-Qur’an.</p>
+            </div>
+            </div>
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>3</h3>
+                <p>Menghantarkan warga sekolah menuju prestasi tertinggi.</p>
+            </div>
+            </div>
+
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>4</h3>
+                <p>Menumbuhkan sikap tanggung jawab dan percaya diri.</p>
+            </div>  
+            </div>
+
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>5</h3>
+                <p>Mewujudkan penerapan kurikulum yang terampil, religius, dinamis, inovatif, pintar, 
+                    dan berwawasan luas dan selalu berperilaku sopan santun kepada orang tua, rekan-rekan, 
+                    guru, tata usaha dan selalu mengucap salam.</p>
+            </div>
+            </div>
+
+        </div>
+        <div class="col-md-6">
             
             <div class="service left-icon probootstrap-animate">
             <div class="icon"><i class="icon-checkmark"></i></div>
             <div class="text">
-                <h3>Asperiores Maxime Modi</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+                <h3>6</h3>
+                <p>Meningkatkan pelayanan serta mengoptimalkan sarana dan prasarana.</p>
+            </div>
+            </div>
+
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>7</h3>
+                <p>Memberikan keterampilan untuk mengikuti Pendidikan lebih lanjut. </p>
             </div>
             </div>
             
             <div class="service left-icon probootstrap-animate">
             <div class="icon"><i class="icon-checkmark"></i></div>
             <div class="text">
-                <h3>Libero Maxime Molestiae</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+                <h3>8</h3>
+                <p>Memberikan pelayanan mata pelajaran mulok melalui Pendidikan akhlak 
+                    mulia / Pendidikan karakter dan BTQ untuk meningkatkan keyakinan dan 
+                    pengalaman agama dalam kehidupan sehari-hari.</p>
+            </div>
+            </div>
+
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>9</h3>
+                <p>Menghantarkan para peserta didik memahami dan mampu menerapkan budaya daerah dalam kehidupan sehari-hari. </p>
+            </div>
+            </div>
+
+            <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+                <h3>10</h3>
+                <p>Menumbuhkan pembiasaan berperilaku agamis dan sopan santun.</p>
             </div>
             </div>
 
